0439a2748492d909b0d728ce54e63865571b8632
Jefferson dos Santos Ferreira <jdsferreira01@gmail.com>
2025-02-18 16:28:17 -0300

    Create variaveis.java

---
+++ variaveis.java
@@ -0,0 +1,12 @@
+public class cursojava
+
+public static void main(String[] args{
+     
+   int n1=10;
+   int n2=20;
+   int n3=30;
+   int res=n1+n2+n3;
+   String nome = "Jefferson";
+
+          System.out.printf("Curso Java!");
+          System.out.prinf("O valor da variavel num:%d%nnome: %s", res, nome); 
