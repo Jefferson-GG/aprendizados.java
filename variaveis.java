650e649c03ab467569041b91bdfffeadbe82bdf5
Jefferson dos Santos Ferreira <jdsferreira01@gmail.com>
2025-02-18 17:35:58 -0300

    Update variaveis.java

--- variaveis.java
+++ variaveis.java
@@ -1,12 +1,11 @@
-public class cursojava
+public class olajava {
+    public static void main(String[] args) {
+        
+        int n1=10, n2=20, n3=30;
+        int res = n1+ n2+ n3;
+        String nome = "Jefferson";
 
-public static void main(String[] args{
-     
-   int n1=10;
-   int n2=20;
-   int n3=30;
-   int res=n1+n2+n3;
-   String nome = "Jefferson";
-
-          System.out.printf("Curso Java!");
-          System.out.prinf("O valor da variavel num:%d%nnome: %s", res, nome); 
+        System.out.println("Ola java");
+        System.err.printf("Ola %s, o valor da variavel resultado é %d\n", nome, res);
+    }
+}
